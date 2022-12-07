b5b39364999ead012cdeb381d25ed354cdf345c0
husita-h <taisei.fukushi1129@gmail.com>
2022-12-08 01:51:45 +0900

    wip: googledriveからファイルを取り出す

---
+++ src/main/copy_files_to_storage/memo.md
@@ -0,0 +1,19 @@
+# memo
+
+## Google APIs Node.js Clientをインストールする
+
+https://github.com/googleapis/google-api-nodejs-client
+
+```sh
+docker compose exec app npm install googleapis
+```
+
+## 参考
+
+- https://cloud.google.com/storage/docs/reference/libraries#client-libraries-usage-nodejs
+- xxx
+
+
+## そのほか
+
+- 関係ないけど、リンター何使ったら良い?
---
+++ src/main/copy_files_to_storage/test.ts
@@ -0,0 +1,31 @@
+const { Storage } = require('@google-cloud/storage');
+import { google } from "googleapis";
+import { Readable } from "stream";
+
+async function copyToCloudStorageAsync(fileId: string, bucketName: string, storageFileName: string, contentType: string): Promise<void> {
+    const authClient = await google.auth.getClient({
+        scopes: "https://www.googleapis.com/auth/drive"
+    });
+    const drive = google.drive({ version: "v3", auth: authClient });
+    const media = await drive.files.get({ fileId, supportsTeamDrives: true, alt: "media" }, { responseType: "stream" });
+
+    const storage = new Storage();
+    const bucket = storage.bucket(bucketName)
+    const uploadFile = bucket.file(storageFileName);
+
+    await new Promise<void>((resolve, reject) => {
+        const downloadStream = media.data as Readable;
+        const uploadStream = uploadFile.createWriteStream({
+        metadata: {
+            cacheControl: "no-cache",
+            contentType
+        }
+        });
+        downloadStream.pipe(uploadStream);
+        downloadStream.on("error", reject);
+        uploadStream.on("error", reject);
+        uploadStream.on("finish", resolve);
+    });
+}
+
+copyToCloudStorageAsync("aaaaaa", "bbbbbb", "cccccc", "dddddd");
