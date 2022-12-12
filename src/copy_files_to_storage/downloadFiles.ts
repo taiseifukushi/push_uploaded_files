e8274e883477b5dc406543cd884c2475d8c95f63
husita-h <taisei.fukushi1129@gmail.com>
2022-12-13 02:24:47 +0900

    ファイルをダウンロード

--- src/copy_files_to_storage/downloadFiles.ts
+++ src/copy_files_to_storage/downloadFiles.ts
@@ -1,65 +1,91 @@
 import { google, drive_v3 } from "googleapis";
 import { driveV3Service } from "./service/driveV3Auth";
+import { GaxiosResponse } from "gaxios";
 import * as fs from "fs"; 
 import { Readable } from "stream";
 import { readFile, writeFile } from "fs/promises";
-import { CreateBucketRequest, Storage } from "@google-cloud/storage";
-
-// type Files = drive_v3.Schema$File[]
+import path from "path";
 
 const listInDrive = async () => {
-    const listParams: drive_v3.Params$Resource$Files$List = {
-        pageSize: 10,
-        fields: "nextPageToken, files(id, name)",
-    };
-    const res = await driveV3Service.files.list(listParams).then((f) => {
-        console.log(f.data.files);
-        return f.data.files;
-    });
-    return res;
-}
-
-const downloadFiles = async () => {
-    // const files = listInDrive();
-    const fileId = "1kCokseof3kuVe5zX5Sn0Au8miwXmPn_e";
-    try {
-        const data = await driveV3Service.files.get({
-            fileId: fileId,
-			alt: "media",
-		});
-        console.log(data)
-        return data.status;
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+	const listParams: drive_v3.Params$Resource$Files$List = {
+		pageSize: 10,
+		fields: "nextPageToken, files(id, name)",
+	};
+	await driveV3Service.files.list(listParams).then((res) => {
+		if (typeof res.data.files == "undefined") {
+			return;
+		}
+		for (const file of res.data.files) {
+			const dest = fs.createWriteStream("./name");
+			// const dest = fs.createWriteStream("./" + file["name"]);
+			if (typeof file == "undefined") {
+				return;
+			}
+			if (file["id"] && file["name"]) {
+				driveV3Service.files.get({
+					fileId: file["id"],
+					alt: "media",
+				},
+				{
+					responseType: "stream",
+				}, (error, res) => {
+					if (res && res.data) {
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+						res.data.pipe(dest);
+					}
+				})
+			}
+		}
+	})
 };
 
-downloadFiles().catch((err) => console.error(err));
 
-const copyToCloudStorageAsync = async (fileId: string, bucketName: string, storageFileName: string, contentType: string): Promise<void> => {
-	const media = await driveV3Service.files.get(
-		{ fileId, supportsTeamDrives: true, alt: "media" },
-		{ responseType: "stream" }
-	);
+// const downloadFiles = async () => {
+// // const downloadFiles = async (): Promise<GaxiosResponse<drive_v3.Schema$File>> => {
+//     const files = listInDrive();
+// 	for (const file in files) {
+// 		// const fileId = "1kCokseof3kuVe5zX5Sn0Au8miwXmPn_e";
+// 		if (typeof file == "undefined") {
+// 			const result =  await driveV3Service.files.get({
+// 				fileId: "1kCokseof3kuVe5zX5Sn0Au8miwXmPn_e",
+// 				alt: "media"
+// 			}).then((res) => {
+// 				console.log(res);
+// 				return res;
+// 			}).catch((err) => {
+// 				return console.error(err);
+// 			});
+// 		}
+// 	}
+// 	return;
+// };
 
-	const storage = new Storage();
-	const bucket = storage.bucket(bucketName);
-	const uploadFile = bucket.file(storageFileName);
+async () => await listInDrive().then((res) => { return res });
+// console.log(aaa);
 
-	await new Promise<void>((resolve, reject) => {
-		const downloadStream = media.data;
-		const uploadStream = uploadFile.createWriteStream({
-			metadata: {
-				cacheControl: "no-cache",
-				contentType,
-			},
-		});
-		downloadStream.pipe(uploadStream);
-		downloadStream.on("error", reject);
-		uploadStream.on("error", reject);
-		uploadStream.on("finish", resolve);
-	});
-}
+// const copyToCloudStorageAsync = async (fileId: string, bucketName: string, storageFileName: string, contentType: string): Promise<void> => {
+// 	const media = await driveV3Service.files.get(
+// 		{ fileId, supportsTeamDrives: true, alt: "media" },
+// 		{ responseType: "stream" }
+// 	);
+
+// 	const storage = new Storage();
+// 	const bucket = storage.bucket(bucketName);
+// 	const uploadFile = bucket.file(storageFileName);
+
+// 	await new Promise<void>((resolve, reject) => {
+// 		const downloadStream = media.data;
+// 		const uploadStream = uploadFile.createWriteStream({
+// 			metadata: {
+// 				cacheControl: "no-cache",
+// 				contentType,
+// 			},
+// 		});
+// 		downloadStream.pipe(uploadStream);
+// 		downloadStream.on("error", reject);
+// 		uploadStream.on("error", reject);
+// 		uploadStream.on("finish", resolve);
+// 	});
+// }
 
 // copyToCloudStorageAsync("aaaaaa", "bbbbbb", "cccccc", "dddddd");
