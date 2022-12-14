35d2e63bdc7a85543d5dd8909a589b0520223fa2
husita-h <taisei.fukushi1129@gmail.com>
2022-12-15 02:30:26 +0900

    No overload matches this call error
    この呼び出しに一致するオーバーロードはありません。
      6 中 1 のオーバーロード, '(params: Params$Resource$Files$Get, options: StreamMethodOptions | BodyResponseCallback<Readable>, callback: BodyResponseCallback<...>): void' により、次のエラーが発生しました。
        型 'string | null | undefined' を型 'string | undefined' に割り当てることはできません。
          型 'null' を型 'string | undefined' に割り当てることはできません。
      6 中 2 のオーバーロード, '(params: Params$Resource$Files$Get, options: MethodOptions | BodyResponseCallback<Schema$File>, callback: BodyResponseCallback<...>): void' により、次のエラーが発生しました。
        型 'string | null | undefined' を型 'string | undefined' に割り当てることはできません。ts(2769)
    v3.d.ts(4516, 9): 予期された型は、型 'Params$Resource$Files$Get' に対してここで宣言されたプロパティ 'fileId' から取得されています
    v3.d.ts(4516, 9): 予期された型は、型 'Params$Resource$Files$Get' に対してここで宣言されたプロパティ 'fileId' から取得されています

--- src/copy_files_to_storage/downloadFiles.ts
+++ src/copy_files_to_storage/downloadFiles.ts
@@ -1,91 +1,47 @@
-import { google, drive_v3 } from "googleapis";
+import { drive_v3 } from "googleapis";
 import { driveV3Service } from "./service/driveV3Auth";
-import { GaxiosResponse } from "gaxios";
 import * as fs from "fs"; 
-import { Readable } from "stream";
-import { readFile, writeFile } from "fs/promises";
-import path from "path";
 
-const listInDrive = async () => {
+const downloadFiles = async (): Promise<void> => {
 	const listParams: drive_v3.Params$Resource$Files$List = {
 		pageSize: 10,
 		fields: "nextPageToken, files(id, name)",
 	};
-	await driveV3Service.files.list(listParams).then((res) => {
-		if (typeof res.data.files == "undefined") {
+	const lists = await driveV3Service.files.list(listParams);
+
+	if (lists.data.files == undefined) {
+		return;
+	}
+	for (const file of lists.data.files) {
+		if (file["mimeType"] != "application/vnd.google-apps.folder") {
 			return;
 		}
-		for (const file of res.data.files) {
-			const dest = fs.createWriteStream("./name");
-			// const dest = fs.createWriteStream("./" + file["name"]);
-			if (typeof file == "undefined") {
-				return;
-			}
-			if (file["id"] && file["name"]) {
-				driveV3Service.files.get({
-					fileId: file["id"],
-					alt: "media",
-				},
-				{
-					responseType: "stream",
-				}, (error, res) => {
-					if (res && res.data) {
-						// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-						res.data.pipe(dest);
-					}
-				})
-			}
+		if (file["name"] == null) {
+			continue;
 		}
-	})
+		if (file["name"] == null || file["name"] == undefined) {
+			continue;
+		}
+		const dest = fs.createWriteStream(
+			`./tmp/uploaded/${file["name"]}`
+		);
+		await driveV3Service.files.get(
+			{
+				fileId: file["id"],
+				alt: "media",
+			},
+			{
+				responseType: "stream",
+			},
+			(r: { data: { pipe: (arg0: fs.WriteStream) => void; }; }) => {
+				{
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+					r.data.pipe(dest);
+				}
+			}
+		)
+	}
+	return;
 };
 
-
-// const downloadFiles = async () => {
-// // const downloadFiles = async (): Promise<GaxiosResponse<drive_v3.Schema$File>> => {
-//     const files = listInDrive();
-// 	for (const file in files) {
-// 		// const fileId = "1kCokseof3kuVe5zX5Sn0Au8miwXmPn_e";
-// 		if (typeof file == "undefined") {
-// 			const result =  await driveV3Service.files.get({
-// 				fileId: "1kCokseof3kuVe5zX5Sn0Au8miwXmPn_e",
-// 				alt: "media"
-// 			}).then((res) => {
-// 				console.log(res);
-// 				return res;
-// 			}).catch((err) => {
-// 				return console.error(err);
-// 			});
-// 		}
-// 	}
-// 	return;
-// };
-
-async () => await listInDrive().then((res) => { return res });
-// console.log(aaa);
-
-// const copyToCloudStorageAsync = async (fileId: string, bucketName: string, storageFileName: string, contentType: string): Promise<void> => {
-// 	const media = await driveV3Service.files.get(
-// 		{ fileId, supportsTeamDrives: true, alt: "media" },
-// 		{ responseType: "stream" }
-// 	);
-
-// 	const storage = new Storage();
-// 	const bucket = storage.bucket(bucketName);
-// 	const uploadFile = bucket.file(storageFileName);
-
-// 	await new Promise<void>((resolve, reject) => {
-// 		const downloadStream = media.data;
-// 		const uploadStream = uploadFile.createWriteStream({
-// 			metadata: {
-// 				cacheControl: "no-cache",
-// 				contentType,
-// 			},
-// 		});
-// 		downloadStream.pipe(uploadStream);
-// 		downloadStream.on("error", reject);
-// 		uploadStream.on("error", reject);
-// 		uploadStream.on("finish", resolve);
-// 	});
-// }
-
-// copyToCloudStorageAsync("aaaaaa", "bbbbbb", "cccccc", "dddddd");
+async () => await downloadFiles();
