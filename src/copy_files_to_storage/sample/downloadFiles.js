ce786c6f538d8eb527fb11a8360238d678656d7f
husita-h <taisei.fukushi1129@gmail.com>
2022-12-14 23:52:21 +0900

    javascriptでサンプル実装

---
+++ src/copy_files_to_storage/sample/downloadFiles.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { google } = require("googleapis");
+const fs = require("fs");
+
+const downloadFiles = async () => {
+	const auth = new google.auth.GoogleAuth({
+		keyFile: path.join("config", "credentials.json"),
+		scopes: ["https://www.googleapis.com/auth/drive"],
+	});
+
+	const drive = google.drive({ version: "v3", auth: auth });
+	const params = { pageSize: 10 };
+	const res1 = await drive.files.list(params);
+
+	for (const file of res1.data.files) {
+		if (file["mimeType"] != "application/vnd.google-apps.folder") {
+			let dest = fs.createWriteStream(`./tmp/uploaded/${file["name"]}`);
+			await drive.files.get(
+				{
+					fileId: file["id"],
+					alt: "media",
+				},
+				{
+					responseType: "stream",
+				},
+				(error, r) => {
+					{
+						r.data.pipe(dest);
+					}
+				}
+				);
+			}
+	}
+	return;
+}
+
+downloadFiles();
