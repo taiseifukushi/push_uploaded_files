90569636e330c8004e8af446d8bdae761b49761d
husita-h <taisei.fukushi1129@gmail.com>
2023-02-11 20:17:11 +0900

    fix: タイポの修正

--- src/push_to_repo/pushToRepo.js
+++ src/push_to_repo/pushToRepo.js
@@ -4,69 +4,47 @@ const ff = require("@google-cloud/functions-framework");
 const path = require("path");
 const fs = require("fs");
 const cwd = path.join(__dirname, "..");
-const  simpleGit = require("simple-git");
+const simpleGit = require("simple-git");
 const repositoryUrl = process.env.RepositoryUrl;
 const repositoryName = process.env.RepositoryName;
 
-function downloadCloudStorageBucketFile(bucketName, fileName, destFileName) {
-	async function downloadFile() {
-		const options = {
-			destination: destFileName,
-		};
-
-		await storage.bucket(bucketName).file(fileName).download(options);
-
-		console.log(
-			`gs://${bucketName}/${fileName} downloaded to ${destFileName}.`
-		);
-		return;
-	}
-
-	downloadFile().catch(console.error);s
+async function downloadCloudStorageBucketFile(bucketName, fileName, destFileName) {
+	const options = {
+		destination: destFileName,
+	};
+	await storage.bucket(bucketName).file(fileName).download(options);
+	console.log(`gs://${bucketName}/${fileName} downloaded to ${destFileName}.`);
 }
 
-async function cloneGitRepostory(repositoryUrl) {
+async function cloneGitRepository(repositoryUrl) {
 	await simpleGit().clone(repositoryUrl, cwd);
-	return;
 }
 
-async function pushGitRepostory(workingDirectory) {
+async function pushGitRepository(workingDirectory) {
 	const git = simpleGit(workingDirectory);
-	const branchName = new Date().toLocaleString().replace(/\/|:| /g, "")
-	const commitmessage = `update_cloud_storage-${branchName}`;
+	const branchName = new Date().toLocaleString().replace(/\/|:| /g, "");
+	const commitMessage = `update_cloud_storage-${branchName}`;
 
-	git.checkoutLocalBranch(branchName);
-	git.add('*')
-		 .commit(commitmessage)
-		 .addRemote('origin', repositoryUrl)
-		 .push(['-u', 'origin', branchName])
+	await git.checkoutLocalBranch(branchName);
+	await git.add("*").commit(commitMessage);
+	await git.addRemote("origin", repositoryUrl);
+	await git.push(["-u", "origin", branchName]);
 }
 
-// bucketの中身を削除する
-function deleteBucketContent(bucketName, fileName) {
-	async function deleteFile() {
-		await storage.bucket(bucketName).file(fileName).delete(deleteOptions);
-		console.log(`gs://${bucketName}/${fileName} deleted`);
-	}
-
-	deleteFile().catch(console.error);
+async function deleteBucketContent(bucketName, fileName) {
+	await storage.bucket(bucketName).file(fileName).delete();
+	console.log(`gs://${bucketName}/${fileName} deleted`);
 }
 
-// Entrypoint
-ff.cloudEvent("pushToRepo", (cloudEvent) => {
+ff.cloudEvent("pushToRepo", async (cloudEvent) => {
 	console.log(`Event ID: ${cloudEvent.id}`, `Event Type: ${cloudEvent.type}`);
 
 	const bucketName = cloudEvent.data.bucket;
 	const bucketFileName = cloudEvent.data.name;
-	const destFileName = path.join(cwd, `${bucketFileName}.txt`);
+	const destFileName = path.join(cwd, `${repositoryName}`, `${bucketFileName}.txt`);
 
-	cloneGitRepostory(repositoryUrl);
-
-	const destFileNamePath = `${cwd}/${repositoryName}/${destFileName}`
-
-	downloadCloudStorageBucketFile(bucketName, bucketFileName, destFileNamePath);
-	pushGitRepostory(repositoryName)
-	deleteBucketContent(bucketName, bucketFileName);
-
-	return;
+	await cloneGitRepository(repositoryUrl);
+	await downloadCloudStorageBucketFile(bucketName, bucketFileName, destFileName);
+	await pushGitRepository(repositoryName);
+	await deleteBucketContent(bucketName, bucketFileName);
 });
